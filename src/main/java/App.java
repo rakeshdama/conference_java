64f1000e02900e764fe2d301026eb81234f774a5
rakeshdama <rakeshd0552@gmail.com>
2021-05-06 23:24:40 +0530

    Using Setter Injection

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,16 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import service.SpeakerService;
-import service.SpeakerServiceImpl;
 
 public class App {
 
     public static void main(String[] args) {
 
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //SpeakerService service = new SpeakerServiceImpl();
+
+        SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
 
---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,22 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import repository.SpeakerRepository;
+import repository.SpeakerRepositoryImpl;
+import service.SpeakerService;
+import service.SpeakerServiceImpl;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "speakerService")
+    public SpeakerService getSpeakerService() {
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
+        service.setRepository(getSpeakerRepository());
+        return service;
+    }
+
+    @Bean(name = "speakerRepository")
+    public SpeakerRepository getSpeakerRepository(){
+        return new SpeakerRepositoryImpl();
+    }
+}
